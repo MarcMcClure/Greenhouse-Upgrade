337164edad7af442cadb40623e83cf51b6c988d6
Marc McClure <mmxman123@gmail.com>
2025-05-16 14:27:23 -0400

    v0.2 micro does a caluclationa and then print the output to the tak moniter via the USB

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,42 @@
 // Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN 2
 
+void testCalculationAndOutput();
+void flashLED();
+
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  testCalculationAndOutput();
 }
 
+
 void loop()
+{
+
+}
+
+// do a caluclation then print the variables and output
+void testCalculationAndOutput()
+{
+  //wait for USB port to connect
+  Serial.begin(9600); //set to default moniter speed
+  while (!Serial);
+
+  int a = 7;
+  int b = 5;
+  int result = a + b;
+
+  Serial.println("Simple Calculation:");
+  Serial.print(a);
+  Serial.print(" + ");
+  Serial.print(b);
+  Serial.print(" = ");
+  Serial.println(result);
+}
+
+// function to flash an LED
+// setup by initializeing LED digital pin as an output with pinMode(LED_BUILTIN, OUTPUT);
+void flashLED()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
